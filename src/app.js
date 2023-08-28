16b367153c698111d7060994281a5ee10208aebc
Miguel <142357205+t0liguemi@users.noreply.github.com>
2023-08-28 03:48:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,46 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const subject = [
+  "The dog",
+  "My grandma",
+  "Madonna",
+  "Barack Obama",
+  "Augusto Pinochet"
+];
+const verb = [
+  "devoured",
+  "eternally doomed",
+  "stared too strongly at",
+  "anihilated"
+];
+const victim = [
+  "all programmers",
+  "my mother",
+  "my left airpod",
+  "Diego Armando Maradona",
+  "all chances of survival"
+];
+const complement = [
+  "right before I hopped into the couch",
+  "while I was having my breakfast",
+  "exactly 2 minutes and 47 seconds before the death of Queen Elizabeth",
+  "because I didn't pray enough last month",
+  "as if it was the most important thing ever to be done"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let chosenSubject = subject[Math.floor(Math.random() * 5)];
+  let chosenVerb = verb[Math.floor(Math.random() * 5)];
+  let chosenVictim = victim[Math.floor(Math.random() * 5)];
+  let chosenComplement = complement[Math.floor(Math.random() * 5)];
+  document.querySelector("#excuse").innerHTML =
+    chosenSubject +
+    " " +
+    chosenVerb +
+    " " +
+    chosenVictim +
+    " " +
+    chosenComplement +
+    ".";
 };
